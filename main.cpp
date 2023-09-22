34b2f79ea32daec5241e394a614a7330f0f6a0a3
pipipuncheck <127033040+pipipuncheck@users.noreply.github.com>
2023-09-22 23:02:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-
-#include "lib.h"
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Bruh: " << endl
-         << A() << endl;
+
+int main(){
+string s, s1;
+cin»s;
+for(int i=s.length()-1, j=0; i>=0; i--, j++){
+s1[j]=s[i];
+}
+for(int i=0; i<s.length(); i++){
+if(s[i]==s1[i]){
+}
+else{
+cout«"no";
+return 0;
+}
+}
+cout«"yes";
 }
